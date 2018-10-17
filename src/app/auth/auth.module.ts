d5a2f440f62aa44bfdb2ec37561f2711e1c65e43
papapin <papapin777#gmail.com>
2018-10-17 12:06:07 -0700

    creating routers

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,14 +4,13 @@ import { CommonModule } from "@angular/common";
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AuthComponent } from './auth.component';
+import { AuthRoutingModule } from "./auth-routing.module";
 
 
 @NgModule({
   declarations: [LoginComponent, RegistrationComponent, AuthComponent],
-  imports: [
-    CommonModule
-   ],
+  imports: [CommonModule, AuthRoutingModule],
   providers: [],
-  bootstrap: [ ]
+  bootstrap: []
 })
 export class AuthModule {}
